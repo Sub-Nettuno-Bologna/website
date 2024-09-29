dcd6f6f80fa750f0771653d999d9591671ea5f55
Carlo <carlo.panzi@gmail.com>
2024-09-29 18:17:54 +0200

    feat: 11ty project

---
+++ web11ty/src/js/leaflet-map.js
@@ -0,0 +1,20 @@
+(function () {
+  const map = L.map('map', { scrollWheelZoom: false }).setView(
+    [44.4921, 11.3086],
+    17
+  );
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 19,
+  }).addTo(map);
+
+  L.marker([44.4922, 11.3086])
+    .addTo(map)
+    .bindPopup("Questo è l'ingresso della sede");
+
+  L.marker([44.493, 11.3082])
+    .addTo(map)
+    .bindPopup('Questa è la piscina Carmen Longo, dove ci alleniamo');
+})();
