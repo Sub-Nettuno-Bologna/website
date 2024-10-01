27c3825e0d00b8c119b4ef5ed318426d3573fa4c
Carlo <carlo.panzi@gmail.com>
2024-10-01 10:46:42 +0200

    fix: sanity read fixes

--- web11ty/src/_data/site.js
+++ web11ty/src/_data/site.js
@@ -17,22 +17,21 @@ const query = `
 
 // Fetch the data from Sanity during the build
 module.exports = async function () {
-  try {
-    const [sanityData] = await client.fetch(query);
+  const [sanityData] = await client.fetch(query);
 
-    const { headerImages, ...data } = sanityData;
-
-    data.headerImages = headerImages.map((img) => {
-      return urlFor(img.asset).height(500).url();
-    });
-
-    //console.log('Sanity data fetched successfully: ', data);
-
-    data.curYear = new Date().getFullYear();
-
-    return data;
-  } catch (err) {
-    console.error('Error fetching Sanity data: ', err);
-    return []; // Return an empty array if something goes wrong
+  if (!sanityData) {
+    throw new Error('No site configuration data found in Sanity');
   }
+
+  const { headerImages, ...data } = sanityData;
+
+  data.headerImages = headerImages.map((img) => {
+    return urlFor(img.asset).height(500).url();
+  });
+
+  //console.log('Sanity data fetched successfully: ', data);
+
+  data.curYear = new Date().getFullYear();
+
+  return data;
 };
